7a375440be8852e898eeae819478dd8fa84c096d
Miljan Pavlovic <miljan.pavlovic@ibm.com>
2023-07-06 12:03:57 +0200

    adding instana collector

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+
+	instana "github.com/instana/go-sensor"
 )
 
 func main() {
+
+	c := instana.InitCollector(&instana.Options{
+		Service: "my-go-app",
+	})
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Ok")
+	}
+
+	http.HandleFunc("/api", instana.TracingHandlerFunc(c.LegacySensor(), "/api", handler))
+	//
 	app := fiber.New()
 	app.Use(cors.New())
 
